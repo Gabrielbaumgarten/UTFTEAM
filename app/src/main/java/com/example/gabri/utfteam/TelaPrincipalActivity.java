9e04935f8b7b7b7f4f54e4f47077b26fa99027a8
TrabalhoAPS <aps>
2019-12-10 16:12:01 -0300

    Adição dos telas do principais no botton navigation

--- app/src/main/java/com/example/gabri/utfteam/TelaPrincipalActivity.java
+++ app/src/main/java/com/example/gabri/utfteam/TelaPrincipalActivity.java
@@ -1,48 +1,66 @@
 package com.example.gabri.utfteam;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
 
-public class TelaPrincipalActivity extends Activity {
+public class TelaPrincipalActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
+    private BottomNavigationView navigationView;
     private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_grade_horaria:
-                    mTextMessage.setText("Grade Horária");
-                    return true;
-                case R.id.navigation_menu:
-                    mTextMessage.setText("Menu");
-                    return true;
-                case R.id.navigation_perfil:
-                    mTextMessage.setText("Perfil");
-                    return true;
-            }
-            return false;
-        }
-    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tela_principal);
 
+        navigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation_view);
+        navigationView.setOnNavigationItemSelectedListener(this);
+
         mTextMessage = (TextView) findViewById(R.id.message);
+        Intent i = getIntent();
+        Usuario usuario = (Usuario) i.getSerializableExtra("objeto");
+        String nome = usuario.getNome();
 //        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
 //        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.navigation_home:
+                Fragment home_fragment = Home_fragment.newInstance();
+                openFragment(home_fragment);
+                break;
+            case R.id.navigation_grade_horaria:
+                Fragment grade_horaria_fragment = Grade_horaria_fragment.newInstance();
+                openFragment(grade_horaria_fragment);
+                break;
+            case R.id.navigation_menu:
+                Fragment menu_fragment = Menu_fragment.newInstance();
+                openFragment(menu_fragment);
+                break;
+            case R.id.navigation_perfil:
+                Fragment perfil_fragment = Perfil_fragment.newInstance();
+                openFragment(perfil_fragment);
+                break;
+        }
+        return false;
+    }
+
+    private void openFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.container, fragment);
+        transaction.addToBackStack(null);
+        transaction.commit();
+    }
+
+
 
 }
